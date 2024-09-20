48040f70dd8ef79616d663f8a9b31a28d577ef45
Tiago Macedo <101218633+TiagodMacedo@users.noreply.github.com>
2024-09-20 01:17:36 +0000

    Resolvendo o defafio dio

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,8 @@
-# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Vamos solicitar uma string e um numero inteiro como entrada. Depois teremos que retornar a string repetida o numero de vezes informado.
+
+def repetir_string():
+    string = input("Digite uma string: ")
+    num_repeticoes = int(input("Digite o número de repetições: "))
+    return (' '.join([string] * num_repeticoes))
+
+print(repetir_string())
---
+++ resolucoes_code/verificando_par_impar.py
@@ -0,0 +1,9 @@
+# Como entrada, receba um número inteiro e verifique se ele é par ou ímpar. Uma dica é: Utilize condicionais para realizar a verificação e, se possível, faça uso do Github Copilot(ou outra IA) para otimizar a estrutura do código.
+
+num1 = int(input("Digite um numero: "))
+
+if num1 % 2 == 0:
+    print("O numero é par")
+else:
+    print("O numero é impar")
+    
